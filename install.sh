a25fe8564b24b02d50ea92ab1c19fda907a5a1b0
rhiroe <ride.poke@gmail.com>
2024-08-22 21:23:23 +0900

    update

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ docker build --pull -f ~/dotfiles/jq.dockerfile -t jq .
 docker build --pull --secret id=aws_config,src=${LOCAL_HOME}/.aws/config --secret id=saml2aws,src=${LOCAL_HOME}/.saml2aws -f ~/dotfiles/saml2aws.dockerfile -t saml2aws .
 docker build --pull -f ~/dotfiles/yq.dockerfile -t yq .
 
+cat <<EOF >> ~/.zshrc
 export PATH=$PATH:$HOME/dotfiles/bin/ecspresso:$HOME/dotfiles/bin/jq:$HOME/dotfiles/bin/saml2aws:$HOME/dotfiles/bin/yq
+EOF
