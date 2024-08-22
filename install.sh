4fcda3e1937197413dae802728430727151580e4
rhiroe <ride.poke@gmail.com>
2024-08-22 21:16:18 +0900

    update

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ docker build --pull -f ~/dotfiles/ecspresso.dockerfile -t ecspresso .
 docker build --pull -f ~/dotfiles/jq.dockerfile -t jq .
 docker build --pull --secret id=aws_config,src=${LOCAL_HOME}/.aws/config --secret id=saml2aws,src=${LOCAL_HOME}/.saml2aws -f ~/dotfiles/saml2aws.dockerfile -t saml2aws .
 docker build --pull -f ~/dotfiles/yq.dockerfile -t yq .
+
+export PATH=$PATH:$HOME/dotfiles/bin/ecspresso:$HOME/dotfiles/bin/jq:$HOME/dotfiles/bin/saml2aws:$HOME/dotfiles/bin/yq
